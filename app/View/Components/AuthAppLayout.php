856fa9b7cf72fbb7fa2c83204388391adac0d6b1
Ahmed AlSardi <A7Dpy@outlook.com>
2023-07-02 14:17:12 +0300

    restructure layout blade

--- app/View/Components/AuthAppLayout.php
+++ app/View/Components/AuthAppLayout.php
@@ -5,13 +5,13 @@ namespace App\View\Components;
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
-class GuestLayout extends Component
+class AuthAppLayout extends Component
 {
     /**
      * Get the view / contents that represents the component.
      */
     public function render(): View
     {
-        return view('layouts.auth.guest');
+        return view('layouts.auth.app');
     }
 }
--- app/View/Components/AuthGuestLayout.php
+++ app/View/Components/AuthGuestLayout.php
@@ -5,13 +5,13 @@ namespace App\View\Components;
 use Illuminate\View\Component;
 use Illuminate\View\View;
 
-class AppLayout extends Component
+class AuthGuestLayout extends Component
 {
     /**
      * Get the view / contents that represents the component.
      */
     public function render(): View
     {
-        return view('layouts.auth.app');
+        return view('layouts.auth.guest');
     }
 }
