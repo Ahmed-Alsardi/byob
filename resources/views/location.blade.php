7ee81d78ffb1fc85ea98659c188e172cc834749d
Ahmed AlSardi <A7Dpy@outlook.com>
2023-07-11 11:57:12 +0300

    fix location view

--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -5,17 +5,17 @@
             @csrf
             <div class="mb-4">
                 <label for="city" class="block text-gray-700 font-bold mb-2">City:</label>
-                <input type="text" id="city" name="city" class="w-full p-2 border border-gray-300 rounded-md" value="{{$location['city']}}" required>
+                <input type="text" id="city" name="city" class="w-full p-2 border border-gray-300 rounded-md" value="{{$location['city'] ?? ''}}" required>
             </div>
 
             <div class="mb-4">
                 <label for="street" class="block text-gray-700 font-bold mb-2">Street:</label>
-                <input type="text" id="street" name="street" class="w-full p-2 border border-gray-300 rounded-md" value="{{ $location['street']}}" required>
+                <input type="text" id="street" name="street" class="w-full p-2 border border-gray-300 rounded-md" value="{{ $location['street'] ?? ''}}" required>
             </div>
 
             <div class="mb-4">
                 <label for="house_number" class="block text-gray-700 font-bold mb-2">House Number:</label>
-                <input type="number" id="house_number" name="house_number" class="w-full p-2 border border-gray-300 rounded-md" value="{{ $location['house_number']}}" required>
+                <input type="number" id="house_number" name="house_number" class="w-full p-2 border border-gray-300 rounded-md" value="{{ $location['house_number'] ?? ''}}" required>
             </div>
 
             <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition-colors">Submit</button>
